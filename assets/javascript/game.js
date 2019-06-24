09438036a93d312055313f702b26d999d0a52861
Ramon Barajas <ramonb621@yahoo.com>
2019-06-23 20:21:07 -0700

    trying to format javascript portion

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,60 @@
-
-let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-
-document.onkeyup = function (event){
-   let userGuess = event.key;
-
-
-   console.log(userGuess);
-//    let computerGuess = options[Math.floor(Math.random() * options.length)];
-
-//    if (userGuess ===  options) {
-//     console.log(element);
+let wins = 0;
+let current = word;
+let text = guess;
+let remaining = remLetters;
+let used = document.onkeyup = function(event) {
+    let userGuess = event.key;
 }
 
+
+let options = ["horse", "rope", "saddle", "saloon", "train", "cowboy", "wagon", "tuberculosis", "cactus", "pistol", "steeple", "sule", "spurs", "boots", "chaps"];
+
+//for selecting random word
+let word = options[Math.floor(Math.random() * options.length)];
+
+//using underscore to show each letter of answer
+let answers = [];
+for (let i = 0; i < word.length; i++){
+    answers[i] = "_";
+}
+// document.onkeyup = function (event){
+//    let userGuess = event.key;
+
+// }
+
+let remLetters = word.length;
+
+////////////////////
+
+while (remLetters > 0 ){
+
+    alert(answers.join(" "));
+
+    let guess = prompt("Click any letter to start!")
+
+    if(guess === null){
+
+        break; //ends game
+
+
+    }else if(guess.length !== 1) {
+
+        alert("Choose only one letter, please!");
+
+    }else{
+
+        for ( let j = 0; j < word.length; j++){
+
+            if (word[j] === guess) {
+
+                answer[j] = guess;
+
+                remLetters--;
+            }
+        }
+    }
+}
+
+alert(answers.join(" "));
+
+alert("Good job! You picked out " + word + " !")
