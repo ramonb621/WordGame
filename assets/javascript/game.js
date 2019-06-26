5686b8769db520fb852dd961dd79a3770ba0d70d
Ramon Barajas <ramonb621@yahoo.com>
2019-06-25 18:06:07 -0700

    onkeyup is working now logging each keystroke

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,12 @@
 
 let options = ["horse", "rope", "saddle", "saloon", "train", "cowboy", "wagon", "tuberculosis", "cactus", "pistol", "steeple", "sule", "spurs", "boots", "chaps"];
+   
+document.onkeyup = function (event){    // * document.onkeyup is picking up individual keystrokes, funtion is picking up the events of the event.key *Event.key was named as a guess variable * The query selector is filling in the event.key variable in the text id class of the html
 
-let userGuess = document.getElementsByClassName("text").innerHTML = [];
+    // console.log(event.key);
+    let guess = event.key; 
+    document.querySelector("#text").innerHTML = guess; 
+}
 
 let session = options[Math.floor(Math.random() * options.length)]; //for selecting random word
 
@@ -9,18 +14,18 @@ let session = options[Math.floor(Math.random() * options.length)]; //for selecti
 let answers = [];
 for (let i = 0; i < session.length; i++){
     answers[i] = "_";
-    document.getElementsByClassName("current").innerHTML = answers;
+    document.querySelector("current").innerHTML = [answers[i]]; // <----I want this to display the                                                          correct letters you're selecting of the answer
 }
+// Trying to use this to get game started
+let remLetters = session.length;
+    if (remLetters > 0 ){
+    document.querySelector("start").innerHTML = "Click any letter to start!";
+    document.onkeyup = function (event){
+        (answers.join(" "));
 
-// let remLetters = session.length;
+    }
+}    
 
-// while (remLetters > 0 ){
-    
-    // onkeyup(answers.join(" "));
-    
-    // document.getElementsByClassName("start").innerHTML = "Click any letter to start!";
-    
-    // }    
     ////////////////////
     
     // document.getElementsByClassName("start").innerHTML = "Click any letter to start!";
