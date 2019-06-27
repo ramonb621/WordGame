8a9e575722d2638f592c4e8b893a76cb46856873
Ramon Barajas <ramonb621@yahoo.com>
2019-06-26 20:27:45 -0700

    updated game set-up on JS page, updated HTML win/lose sections, added image, and updated stylesheet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,31 +10,42 @@ let guessingWord = []; //word the player is trying to match
 
 let remainingGuesses = 0;  //lives left
 
-let starting = false;  // game start
+let starting = false;  // game status, active or not
 
 let finished = false; // command for "press any key to start!"
 
 let wins = 0;  // total wins
 
 
-// ****************** Starting game code here ***********************
+// ****************** game set up ***********************
 
 function reset (){
+
     remainingGuesses = maxAttempts;
 
     starting = false;
     
     currentWordsIndex = options[Math.floor(Math.random() * options.length)]; //for selecting random word
 
-    lettersGuessed = [];
+    lettersGuessed = []; 
 
     guessingWord = [];
 
     // document.getElementById("hangman").src = "assets0/images/hangman.png";
-
     
+    for(i = 0; i < options[currentWordsIndex].length; i++);{// I'm nesting the currentWordsIndex 
+    guessingWord.push("_");     // variable within the main array.  I needed a way to clear the array                      //and save the word being guessed while that current word being disguised by "_".
 }
+    document.getElementById("hangman").style.cssText="display:none";
    
+    document.getElementsByClassName("try-again").style.cssText= "display: none"; //display:none works...
+
+    document.getElementsByClassName("game-over").style.cssText = "display: none"; //as a css attr...
+    
+    document.getElementsByClassName("congrats").style.cssText = "display: none"; //JS is longer...
+
+    updateDisplay();
+};
 // document.onkeyup = function (event){    // * document.onkeyup is picking up individual keystrokes, funtion is picking up the events of the event.key *Event.key was named as a guess variable * The query selector is filling in the event.key variable in the text id class of the html
 
     // console.log(event.key);
